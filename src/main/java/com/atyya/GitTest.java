38cf45c54e6d274fa45224db36b1e472bb37f5c7
yya <2153271051@qq.com>
2024-06-02 22:33:39 +0800

    hot-fix commit01

--- src/main/java/com/atyya/GitTest.java
+++ src/main/java/com/atyya/GitTest.java
@@ -5,6 +5,7 @@ public class GitTest {
         System.out.println("hello,git");
         System.out.println("hello,git01");
         System.out.println("hello,git03");
+        System.out.println("hello,git04");
 
 
     }
