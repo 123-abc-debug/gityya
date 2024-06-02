72d505cddded658bd29d8387ff343f7dba9157e9
yya <2153271051@qq.com>
2024-06-02 22:22:13 +0800

    second commit

--- src/main/java/com/atyya/GitTest.java
+++ src/main/java/com/atyya/GitTest.java
@@ -3,6 +3,7 @@ package com.atyya;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,git");
+        System.out.println("hello,git01");
 
 
     }
